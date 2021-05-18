93a393dbd16a7927900e94be86809dbf66e11169
minibhalla <mini@lodgable.com>
2021-05-18 13:52:46 +0530

    update one

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const express=require("express");
-
 const app=express();
+const path = require("path");
+const hbs = require("hbs");
 
 const port=process.env.PORT ||3000;
 
+const staticPath = path.join(__dirname, "./");
+const views = path.join(__dirname, "./templates/views");
+
+
+app.set("view engine", "hbs");
+app.set("views", views);
+app.use(express.static(staticPath));
 app.get("/",(req,res)=>{
  
-    res.json("Hello uruz I am sorry!!")
+    res.render("home")
 
 })
 app.listen(port,()=>{
